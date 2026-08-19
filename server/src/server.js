bd93c3e1461ab2535fecd27949fe0914ff30797c
dineshgaikwad0503-ship-it <dineshgaikwad0503@gmail.com>
2026-08-19 14:45:47 +0000

    comm

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const http = require('http');
 const app = require('./app');
 const { connectDB } = require('./config/db');
@@ -6,11 +7,25 @@ const { initSocket } = require('../../socket/socket');
 
 const PORT = process.env.PORT || 5000;
 
+// 👇 यह नया route यहाँ add करो
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'StudySync API is running',
+    port: PORT
+  });
+});
+
 (async () => {
   await connectDB();
+
   const server = http.createServer(app);
+
   initSocket(server);
-  server.listen(PORT, () => console.log(`StudySync API running on ${PORT}`));
+
+  server.listen(PORT, () => {
+    console.log(`StudySync API running on ${PORT}`);
+  });
 })().catch(err => {
   console.error(err);
   process.exit(1);
