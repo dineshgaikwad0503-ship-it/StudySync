29ef7d75be364e3df7f8b0c5ab308a3430dc355b
dineshgaikwad0503-ship-it <dineshgaikwad0503@gmail.com>
2026-08-20 05:15:08 +0000

    fix: use port 5001

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const { connectDB } = require('./config/db');
 const { initSocket } = require('../../socket/socket');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // 👇 यह नया route यहाँ add करो
 app.get('/', (req, res) => {
